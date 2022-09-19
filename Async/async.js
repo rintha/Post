787ff9abf519ae7e0f47b68488680ee688c047e1
rintha <rintharithu@gmail.com>
2022-09-19 21:48:30 +0530

    manali

--- Async/async.js
+++ Async/async.js
@@ -1,46 +1,44 @@
-console.log('Person1 : shows ticket');
-console.log('Person2 : shows ticket');
-
-const preMovie = async ()=>{
-    const promiseWifeBringingTickets=new Promise((resolve,reject)=>{
-        setTimeout(() => resolve('ticket'), 3000);
-    
-    });
-
-    const getPopcorn = new Promise((resolve, reject) => 
-    resolve(`popcorn`));
-
-    const addButter = new Promise((resolve, reject) => 
-    resolve(`Butter`));
-
-    const getColdDrinks = new Promise((resolve, reject) => 
-    resolve(`Cold drinks`));
-
-    let ticket = await promiseWifeBringingTickets;
-
-    console.log(`wife : I have the ${ticket}`);
-    console.log('husband :we should go in');
-    console.log('wife : no I am hungry');
-
-    let popcorn = await getPopcorn;
-    
-    console.log(`husband : I got some ${popcorn}`);
-    console.log('husband :we should go in');
-    console.log('wife : I need butter on my popcorn');
-
-    let butter = await addButter;
-
-    console.log(`husband : I got some ${butter} on popcorn`);
-    console.log('husband : anything else darling?');
-    console.log('wife : can we have  some cold drinks');
-
-    let drinks = await getColdDrinks;
-    console.log('wife : lets got we are getting late');
-    console.log('husband:thank you for the reminder');
-    
-    return ticket
+function buyCar(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+         resolve('buy a car')
+        }, 1000)
+    })
 }
-preMovie().then((m)=>console.log(`person3:shows the ${m}`));
 
-console.log('Person4 : shows ticket');
-console.log('Person5 : shows ticket');
+function planTrip(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(`Lets go to Manali`)
+        }, 2000);
+    })
+}
+
+function reachedManali(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(`reached Manali`)
+        }, 2000);
+    })
+}    
+
+function reachedMountain(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(`reached Mountain`)
+        }, 2000);
+    })
+}
+
+async function fun(){
+    const msg = await buyCar();
+    console.log(msg);
+    const msg1 = await planTrip();
+    console.log(msg1)
+    const msg2 = await reachedManali();
+    console.log(msg2)
+    const msg3 = await reachedMountain();
+    console.log(msg3)
+
+}
+fun()
